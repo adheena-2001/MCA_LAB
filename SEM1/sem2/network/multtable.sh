985e488a723b828d0b859fc1c54bea282a8a140b
Adheena Antony <121556867+adheena-2001@users.noreply.github.com>
2024-03-22 15:13:03 +0530

    Delete SEM1/sem2/network directory

--- SEM1/sem2/network/multtable.sh
+++
@@ -1,7 +0,0 @@
-read -p "Enter the number : " number
-i=1
-while [ $i -le 10 ]
-do
-echo " $number * $i =`expr $number \* $i ` "
-i=`expr $i + 1`
-done
--- SEM1/sem2/network/sum.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Please enter first number"
-read a
-echo "Please enter second number"
-read b
-s=$(($a + $b))
-echo "Sum=" $s
